44a9f97212bea274f947822cf8c12b8faac5248b
Sikori <samuel.czech@inter-media.eu>
2020-04-25 00:42:23 +0200

    - created purchase context

---
+++ einkaufsliste-api/Models/PurchaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace einkaufsliste_api.Models
+{
+    public class PurchaseContext : DbContext
+    {
+        public PurchaseContext(DbContextOptions<PurchaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PurchaseItem> PurchaseItems { get; set; }
+    }
+}
---
+++ einkaufsliste-api/Models/PurchaseItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace einkaufsliste_api.Models
+{
+    public class PurchaseItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsPurchased { get; set; }
+    }
+}
